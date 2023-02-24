a2382ac23cc7cc2221b9c5c81ea695b8409270b0
amauryjaphet <115047834+amauryjaphet@users.noreply.github.com>
2023-02-24 17:36:57 -0600

    improoved evrything

--- donut.py
+++ donut.py
@@ -1,65 +1,81 @@
-import pygame as donut
-import math as albert_einstein
+import pygame
+import math
+import colorsys
 
-donut.init()
-
-width = 1920
-height = 1080
+pygame.init()
 
 white = (255, 255, 255)
 black = (0, 0, 0)
+hue = 0
+
+WIDTH = 1920
+HEIGHT = 1080
 
 x_start, y_start = 0, 0
-x_separator, y_separator = 10, 20
 
-rows = height // y_separator
-columns = width // x_separator
+x_separator = 10
+y_separator = 20
+
+rows = HEIGHT // y_separator
+columns = WIDTH // x_separator
 screen_size = rows * columns
 
-x_offset, y_offset = columns // 2, rows // 2
+x_offset = columns / 2
+y_offset = rows / 2
 
-A, B = 0, 0
+A, B = 0, 0  # rotating animation
 
 theta_spacing = 10
-pi_spacing = 1
-chars = ".,-~:;=!*#$@"
+phi_spacing = 1 # for faster rotation change to 2, 3 or more, but first change 86, 87 lines as commented
 
-screen = donut.display.set_mode((width, height))
+chars = ".,-~:;=!*#$@"  # luminance index
 
-display_surface = donut.display.set_mode((width, height))
-donut.display.set_caption("donut aaaaaaaa")
-font = donut.font.SysFont('Helvetica', 10, bold=True)
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+display_surface = pygame.display.set_mode((WIDTH, HEIGHT))
+# display_surface = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+pygame.display.set_caption('Donut')
+font = pygame.font.SysFont('Arial', 18, bold=True)
+
+def hsv2rgb(h, s, v):
+    return tuple(round(i * 255) for i in colorsys.hsv_to_rgb(h, s, v))
+
 
 def text_display(letter, x_start, y_start):
-    text = font.render(str(letter), True, white)
+    text = font.render(str(letter), True, hsv2rgb(hue, 1, 1))
     display_surface.blit(text, (x_start, y_start))
+
+# def text_display(letter, x_start, y_start):
+#     text = font.render(str(letter), True, white)
+#     display_surface.blit(text, (x_start, y_start))
+
 
 run = True
 while run:
+
     screen.fill((black))
 
-    z = [0] * screen_size
-    b = [' '] * screen_size
+    z = [0] * screen_size  # Donut. Fills donut space
+    b = [' '] * screen_size  # Background. Fills empty space
 
-    for i in range(0, 628, theta_spacing):
-        for j in range(0, 628, pi_spacing):
-            c = albert_einstein.sin(i)
-            d = albert_einstein.cos(j)
-            e = albert_einstein.sin(A)
-            f = albert_einstein.sin(j)
-            g = albert_einstein.cos(A)
+    for j in range(0, 628, theta_spacing):  # from 0 to 2pi
+        for i in range(0, 628, phi_spacing):  # from 0 to 2pi
+            c = math.sin(i)
+            d = math.cos(j)
+            e = math.sin(A)
+            f = math.sin(j)
+            g = math.cos(A)
             h = d + 2
             D = 1 / (c * h * e + f * g + 5)
-            l = albert_einstein.cos(i)
-            m = albert_einstein.cos(B)
-            n = albert_einstein.sin(B)
+            l = math.cos(i)
+            m = math.cos(B)
+            n = math.sin(B)
             t = c * h * g - f * e
-            x = int(x_offset + 40 * D * (l * h * m - t * n))
-            y = int(y_offset + 20 * D * (l * h * n + t * m))
+            x = int(x_offset + 40 * D * (l * h * m - t * n))  # 3D x coordinate after rotation
+            y = int(y_offset + 20 * D * (l * h * n + t * m))  # 3D y coordinate after rotation
             o = int(x + columns * y)
-            N = int(8 * ((f * e - c * d * g) * m - c * d * e - f * g - l * d * n))
-
-            if rows > y and y> 0 and x> 0 and columns > x and D > z[o]:
+            N = int(8 * ((f * e - c * d * g) * m - c * d * e - f * g - l * d * n))  # luminance index
+            if rows > y and y > 0 and x > 0 and columns > x and D > z[o]:
                 z[o] = D
                 b[o] = chars[N if N > 0 else 0]
 
@@ -67,19 +83,26 @@ while run:
         y_start = 0
 
     for i in range(len(b)):
-        A += 0.000002
-        B += 0.000001
+        A += 0.00004  # for faster rotation change to bigger value
+        B += 0.00002  # for faster rotation change to bigger value
         if i == 0 or i % columns:
             text_display(b[i], x_start, y_start)
-            x_start += y_separator
+            x_start += x_separator
         else:
             y_start += y_separator
-            x_start = o
+            x_start = 0
             text_display(b[i], x_start, y_start)
             x_start += x_separator
 
-    donut.display.update()
 
-    for event in donut.event.get():
-        if event.type == donut.QUIT:
+    pygame.display.update()
+
+    hue += 0.005
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
             run = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                run = False
+
